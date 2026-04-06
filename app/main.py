6a2b1e0b21dd3c7f17590b19b31aa77661180214
Mirash1l1 <killer28392@gmail.com>
2026-04-06 21:42:36 +0300

    Внёс изменения по просьюбе Luce

--- app/main.py
+++ app/main.py
@@ -21,4 +21,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(item, linter_report[item]) for item in linter_report]
+    return [format_single_linter_file(file_path, linter_report[file_path]) for file_path in linter_report]
